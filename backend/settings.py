ebd6ec249392a44e4569f42f91c75573f6677629
visheshdvivedi <visheshdvivedi5012@gmail.com>
2024-08-15 09:16:20 +0530

    update backend url

--- backend/settings.py
+++ backend/settings.py
@@ -134,9 +134,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.Account'
 
 # all origin related updates
-ALLOWED_HOSTS = ['localhost', 'leetclone-backend.azurewebsites.com']
+ALLOWED_HOSTS = ['localhost', 'leetclone-backend.azurewebsites.net']
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://leetclone-frontend.vercel.app"]
-CSRF_TRUSTED_ORIGINS = ["https://leetclone-backend.azurewebsites.com"]
+CSRF_TRUSTED_ORIGINS = ["https://leetclone-backend.azurewebsites.net"]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
