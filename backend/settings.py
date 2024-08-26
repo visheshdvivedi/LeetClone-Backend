3fcec21af0c40f27dfea82b74138f2e544cdf4d7
visheshdvivedi <visheshdvivedi5012@gmail.com>
2024-08-26 12:35:18 +0530

    Update ALLOWED_HOSTS

--- backend/settings.py
+++ backend/settings.py
@@ -141,7 +141,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = 'accounts.Account'
 
 # all origin related updates
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["leetclone-backend.azurewebsites.net"]
 CORS_ALLOWED_ORIGINS = ["http://localhost:5173", "https://leetclone-frontend.vercel.app"]
 CSRF_TRUSTED_ORIGINS = ["https://leetclone-backend.azurewebsites.net"]
 
