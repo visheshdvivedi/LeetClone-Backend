12f23fde6f8a9d1ecd85ce735ded364592f37f61
visheshdvivedi <visheshdvivedi5012@gmail.com>
2024-08-26 20:09:21 +0530

    update static files serving

--- backend/urls.py
+++ backend/urls.py
@@ -5,6 +5,8 @@ from rest_framework_simplejwt.views import (
     TokenRefreshView,
     TokenVerifyView
 )
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -13,4 +15,4 @@ urlpatterns = [
     path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
